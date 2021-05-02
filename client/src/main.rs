47c0c89b8057cc5091b0e1f8ebc9f29d8e28ed49
luca <b.luca0815@gmail.com>
2021-05-02 14:07:42 +0200

    first steps into pushing messages to server

--- client/src/main.rs
+++ client/src/main.rs
@@ -7,27 +7,32 @@ use std::net::{TcpStream};
 use std::io::{Read, Write};
 use std::str::from_utf8;
 
+static PORT: &str = "3333";
+
 fn main() {
     let mut terminal = Terminal::init(Size::new(100, 50));
     
-    let mut main_window = Window::new(Position::new(2, 2), Size::new(30, 20));
+    let mut main_window = Window::new(Position::new(0, 0), Size::new(30, 20));
 
-    let mut login_window = Window::new(Position::new(10, 12), Size::new(50, 26));
+    let mut login_window = Window::new(Position::new(0, 0), Size::new(50, 26));
     login_window.set_border_color(Color::red());
 
-    let mut chat_window = Window::new(Position::new(10, 12), Size::new(50, 26));
+    let mut chat_window = Window::new(Position::new(0, 0), Size::new(50, 26));
+    let mut chat_input_window = Window::new(Position::new(0, 0), Size::new(50, 3));
 
     let mut token: [u8; 8192] = [0; 8192];
     let mut status = String::new();
 
     let mut window: u32 = 0;
 
+    let mut input: Vec<char> = Vec::new();
+    let mut messages: Vec<String> = Vec::new();
     loop {
+        terminal.clear();
+        terminal.clear_windows();
+        terminal.update();
+        
         if window == 0 {
-            terminal.clear();
-            terminal.clear_windows();
-            terminal.update();
-
             main_window.clear();
             main_window.center(&terminal);
 
@@ -50,10 +55,6 @@ fn main() {
         }
 
         if window == 1 {
-            terminal.clear();
-            terminal.clear_windows();
-            terminal.update();
-            
             login_window.center(&terminal);
 
             login_window.clear();
@@ -86,17 +87,58 @@ fn main() {
         } 
 
         if window == 2 {
+            chat_window.center(&terminal);
+            chat_window.clear();
+            chat_window.decorate();
 
+            for i in 0..messages.len() {
+                chat_window.write(Position::new(1, i as i32 + 1), messages[i].clone(), Color::white());
+            }
+
+            chat_input_window.clear();
+            chat_input_window.decorate();
+            chat_input_window.set_position(Position::new(
+                chat_window.pos.x,
+                chat_window.pos.y + chat_window.size.y
+            ));
+
+            let input_string: String = input.iter().cloned().collect();
+            chat_input_window.write(Position::new(1, 1), input_string.clone(), Color::white());
+
+            terminal.write_window(&chat_window);
+            terminal.write_window(&chat_input_window);
+
+            terminal.render_accurate();
+
+            match terminal.key_event.code {
+                KeyCode::Char(c) => {
+                    input.push(c);
+                }
+                KeyCode::Enter => {
+                    messages.push(input_string.clone());
+                    input.clear();
+                }
+                KeyCode::Backspace => {
+                    input.pop();
+                }
+                _ => ()
+            }
+
+            match input_string.clone().as_str() {
+                "exit" => break,
+                _ => ()
+            }
         }
+        //thread::sleep(Duration::from_millis(16));
     }
     terminal.quit();
 }
 
 fn check_login(ip: String, name: String, password: String) -> Result<[u8; 8192], String> {
-    match TcpStream::connect(ip + ":3333") {
+    match TcpStream::connect(ip + ":" + PORT) {
         Ok(mut stream) => {
 
-            let mut msg = create_login(name, password);
+            let msg = create_login(name, password);
 
             stream.write(&msg).unwrap();
 
@@ -122,6 +164,34 @@ fn check_login(ip: String, name: String, password: String) -> Result<[u8; 8192],
     return Err("connection terminated".to_string());
 }
 
+fn write_message(ip: String, message: String) -> Result<(), String> {
+    match TcpStream::connect(ip + ":" + PORT) {
+        Ok(mut stream) => {
+            let msg = create_message(message.clone());
+        
+            stream.write(&msg).unwrap();
+
+            let mut data = [0 as u8; 8192];
+            match stream.read_exact(&mut data) {
+                Ok(_) => {
+                    if data == [0 as u8; 8192] {
+                        return Err("Invalid Token".to_string());
+                    }
+                    else {
+                        return Ok(());
+                    }
+                }
+                Err(_) => {
+                    return Err("Failed to recieve data".to_string());
+                }
+            }
+        },
+        Err(_) => {
+            return Err("connection terminated".to_string());
+        }
+    }
+}
+
 fn create_login(name: String, password: String) -> [u8; 8192] {
     let mut buffer = [0x00 as u8; 8192];
     buffer[0] = 0x01;
@@ -130,6 +200,15 @@ fn create_login(name: String, password: String) -> [u8; 8192] {
     }
     for i in 0..password.len() {
         buffer[i + 1 + 4096] = password.as_bytes()[i];
+    }
+    buffer
+}
+
+fn create_message(message: String) -> [u8; 8192] {
+    let mut buffer = [0x00 as u8; 8192];
+    buffer[0] = 0x05;
+    for i in 0..message.len() {
+        buffer[i + 1] = message.as_bytes()[i];
     }
     buffer
 }
